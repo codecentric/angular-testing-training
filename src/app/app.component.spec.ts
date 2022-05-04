bd2a6f1bcb7cb01a20ca574d22667b86e11f6796
Andreas Houben <andreas.houben@codecentric.de>
2022-05-04 21:41:32 +0200

    prepare exercise-3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,11 +4,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { SearchfieldComponent } from './components/searchfield/searchfield.component';
 import { SubmitbuttonComponent } from './components/submitbutton/submitbutton.component';
 import { CharacterService } from './services/character.service';
-import { of } from 'rxjs';
 import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
-  let mockCharacterService: CharacterService;
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
 
@@ -16,9 +14,8 @@ describe('AppComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent, SearchfieldComponent, SubmitbuttonComponent],
       imports: [HttpClientModule, ReactiveFormsModule],
+      providers: [CharacterService],
     }).compileComponents();
-
-    mockCharacterService = TestBed.inject(CharacterService);
   });
 
   beforeEach(() => {
@@ -26,90 +23,18 @@ describe('AppComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-  const mockObject: any = Object.seal({ foo: 'bar' });
 
-  it('should query for a given character', () => {
-    spyOn(mockCharacterService, 'searchForCharacter').and.returnValue(
-      of(mockObject)
-    );
+  it('should query for a given character', () => {});
 
-    component.onSearch('rick');
+  it('fetches the next link from the current page', () => {});
 
-    expect(mockCharacterService.searchForCharacter).toHaveBeenCalledOnceWith(
-      'rick'
-    );
-    expect(component.currentPage).toEqual(mockObject);
-  });
+  it('fetches previous link from the current page', () => {});
 
-  it('fetches the next link from the current page', () => {
-    let next = 'mockUrl';
-    component.currentPage = {
-      info: {
-        next,
-        count: 1,
-        pages: 1,
-        prev: null,
-      },
-    };
+  it('returns false for "moreCharactersAvailable" if currentPage is undefined', () => {});
+  it('returns true for "moreCharactersAvailable" if currentPage has next page', () => {});
+  it('returns true for "moreCharactersAvailable"  if currentPage has prev page', () => {});
 
-    spyOn(mockCharacterService, 'getPage').and.returnValue(of(mockObject));
+  it('returns empty array of characters if currentPage is undefined', () => {});
 
-    component.fetchNext();
-    expect(mockCharacterService.getPage).toHaveBeenCalledOnceWith(next);
-    expect(component.currentPage).toEqual(mockObject);
-  });
-
-  it('fetches previous link from the current page', () => {
-    let prev = 'mockUrl';
-    component.currentPage = {
-      info: {
-        next: null,
-        count: 1,
-        pages: 1,
-        prev,
-      },
-    };
-
-    spyOn(mockCharacterService, 'getPage').and.returnValue(of(mockObject));
-
-    component.fetchPrev();
-    expect(mockCharacterService.getPage).toHaveBeenCalledOnceWith(prev);
-    expect(component.currentPage).toEqual(mockObject);
-  });
-
-  it('returns false for "moreCharactersAvailable" if currentPage is undefined', () => {
-    component.currentPage = undefined;
-
-    expect(component.moreCharsAvailable).toBeFalse();
-  });
-  it('returns true for "moreCharactersAvailable" if currentPage has next page', () => {
-    component.currentPage = {
-      info: {
-        next: 'someUrl',
-      } as Info,
-    };
-
-    expect(component.moreCharsAvailable).toBeTrue();
-  });
-  it('returns true for "moreCharactersAvailable"  if currentPage has prev page', () => {
-    component.currentPage = {
-      info: {
-        prev: 'someUrl',
-      } as Info,
-    };
-
-    expect(component.moreCharsAvailable).toBeTrue();
-  });
-
-  it('returns empty array of characters if currentPage is undefined', () => {
-    component.currentPage = undefined;
-
-    expect(component.charactersOnPage).toEqual([]);
-  });
-
-  it('returns array of characters if currentPage is defined and has characters', () => {
-    component.currentPage = mockObject;
-
-    expect(component.charactersOnPage).toEqual(mockObject.results);
-  });
+  it('returns array of characters if currentPage is defined and has characters', () => {});
 });
