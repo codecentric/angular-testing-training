db2ab923a2cb1523139fa81a8863cbc28b32cb92
Andreas Houben <andreas.houben@codecentric.de>
2022-05-05 21:27:54 +0200

    advanced tooling

--- src/app/components/searchfield/searchfield.component.spec.ts
+++ src/app/components/searchfield/searchfield.component.spec.ts
@@ -1,9 +1,14 @@
-import {SearchfieldComponent} from './searchfield.component';
-import {SubmitbuttonComponent} from '../submitbutton/submitbutton.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {DebugElement} from '@angular/core';
-import {render, RenderResult, screen, waitFor} from "@testing-library/angular";
-import userEvent from "@testing-library/user-event";
+import { SearchfieldComponent } from './searchfield.component';
+import { SubmitbuttonComponent } from '../submitbutton/submitbutton.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DebugElement } from '@angular/core';
+import {
+  render,
+  RenderResult,
+  screen,
+  waitFor,
+} from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 
 function typeInto(inputElement: DebugElement, value: string) {
   inputElement.nativeElement.value = value;
@@ -15,25 +20,21 @@ function typeInto(inputElement: DebugElement, value: string) {
 }
 
 describe('SearchfieldComponent', () => {
+  let renderResult: RenderResult<SearchfieldComponent>;
 
-  let renderResult: RenderResult<SearchfieldComponent>
-
-  const submitValue = jest.fn()
+  const submitValue = jest.fn();
 
   beforeEach(async () => {
     renderResult = await render(SearchfieldComponent, {
       componentProperties: {
         submitted: {
-          emit: submitValue
-        }
+          emit: submitValue,
+        },
       } as any,
       declarations: [SubmitbuttonComponent],
-      imports: [ReactiveFormsModule]
+      imports: [ReactiveFormsModule],
     });
-
-
   });
-
 
   it('renders an input field of type text', () => {
     let input = screen.getByLabelText('Search for');
@@ -44,22 +45,22 @@ describe('SearchfieldComponent', () => {
   it('emits the submitted value', () => {
     let input = screen.getByLabelText('Search for');
 
-    userEvent.type(input, 'search value')
+    userEvent.type(input, 'search value');
 
-    userEvent.click(screen.getByTestId('custom-submit-button'))
+    userEvent.click(screen.getByTestId('custom-submit-button'));
 
-    waitFor(() => expect(submitValue).toHaveBeenCalledWith('search value'))
+    waitFor(() => expect(submitValue).toHaveBeenCalledWith('search value'));
   });
   //
   it('clears the search field after submit', () => {
     let input = screen.getByLabelText('Search for');
 
     let value = 'search value';
-    userEvent.type(input, value)
+    userEvent.type(input, value);
 
-    userEvent.click(screen.getByTestId('custom-submit-button'))
+    userEvent.click(screen.getByTestId('custom-submit-button'));
 
-    expect(input).toHaveDisplayValue('')
+    expect(input).toHaveDisplayValue('');
   });
 
   it('disables the submit button when the input is empty', () => {
--- src/app/components/submitbutton/submitbutton.component.spec.ts
+++ src/app/components/submitbutton/submitbutton.component.spec.ts
@@ -1,30 +1,24 @@
-import {SubmitbuttonComponent} from './submitbutton.component';
-import {createComponentFactory, Spectator} from "@ngneat/spectator/jest";
-
+import { SubmitbuttonComponent } from './submitbutton.component';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
 
 describe('SubmitbuttonComponent', () => {
+  let spectator: Spectator<SubmitbuttonComponent>;
 
-  let spectator: Spectator<SubmitbuttonComponent>
-
-
-  const createComponent = createComponentFactory(SubmitbuttonComponent)
-
+  const createComponent = createComponentFactory(SubmitbuttonComponent);
 
   beforeEach(async () => {
-    spectator = createComponent()
+    spectator = createComponent();
   });
-
 
   it('renders a submit button', () => {
     let button = spectator.query('[data-testid="custom-submit-button');
-
 
     expect(button?.getAttribute('type')).toEqual('submit');
   });
 
   it('shows displays the text passed by the "text" property', () => {
-    spectator.component.text = 'click me'
-    spectator.detectChanges()
+    spectator.component.text = 'click me';
+    spectator.detectChanges();
 
     let button = spectator.query('[data-testid="custom-submit-button');
 
@@ -32,23 +26,20 @@ describe('SubmitbuttonComponent', () => {
   });
 
   it('emits a clicked event on click', () => {
-
     let clicked = false;
     spectator.component.clicked.subscribe(() => (clicked = true));
 
     let button = spectator.query('[data-testid="custom-submit-button');
 
-    spectator.click(button!)
+    spectator.click(button!);
     expect(clicked).toBeTruthy();
   });
 
   it('can be disabled by setting disabled to true', () => {
     spectator.component.disabled = true;
-    spectator.detectChanges()
-
+    spectator.detectChanges();
 
     let button = spectator.query('[data-testid="custom-submit-button');
-
 
     expect(button?.getAttribute('disabled')).not.toBeNull();
   });
